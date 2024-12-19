91fa51cc264662a434a85603825550b658fbc8d8
Neha_E <162294545+Neha-chow@users.noreply.github.com>
2024-12-19 22:08:10 +0530

    Time: 31 ms (55.97%), Space: 51.7 MB (44.62%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,39 @@
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> l = new ArrayList<>();
-        for(int i=0;i<nums.length-2;i++){
-           int  p1=i+1;
-            int p2=nums.length-1;
-            boolean b = false;
-        while(p1<p2){
-            int csum = nums[i]+nums[p1] +nums[p2];
-            if(csum == 0){
-                if(l.contains(Arrays.asList(nums[i], nums[p1], nums[p2]))
-                    b = true;
-                    else{
-                 l.add(Arrays.asList(nums[i], nums[p1], nums[p2]));
-                 while (p1 < p2 && nums[p1] == nums[p1 + 1]) p1++; 
+        List<List<Integer>> result = new ArrayList<>();
+        
+       
+        for (int i = 0; i < nums.length - 2; i++) {
+            
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int p1 = i + 1;
+            int p2 = nums.length - 1;
+           
+            while (p1 < p2) {
+            
+                int sum = nums[i] + nums[p1] + nums[p2];
+                
+                if (sum == 0) {
+                   
+                    result.add(Arrays.asList(nums[i], nums[p1], nums[p2]));
+                    
+                   
+                    while (p1 < p2 && nums[p1] == nums[p1 + 1]) p1++; 
                     while (p1 < p2 && nums[p2] == nums[p2 - 1]) p2--; 
                     
                     p1++;
                     p2--;
-                    }
-              }
-            
-            else if (csum<0)
-            p1++;
-            else p2--;
-          }
+                } else if (sum < 0) {
+                    p1++;
+                } else {
+                    p2--; 
+                }
+            }
         }
-        return l;
+        
+        return result;
     }
 }
