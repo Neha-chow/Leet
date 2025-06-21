3d7fc9889244a75a54618def05d03077dadf1f9f
Neha_E <162294545+Neha-chow@users.noreply.github.com>
2025-06-21 20:06:15 +0530

    Time: 7 ms (6.99%), Space: 43.5 MB (78.08%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -4,9 +4,17 @@ class Solution {
         for(int i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
                 if(nums1[i]==nums2[j]){
+                    if(j==nums2.length-1){
+                        ans[k]=-1;
+                    }
+                    else{
                     for(int p=j+1;p<nums2.length;p++){
-                        if(nums1[i]<nums2[p]) ans[k]=nums2[p];
+                        if(nums1[i]<nums2[p]){
+                            ans[k]=nums2[p];
+                            break;
+                        }
                         else ans[k]=-1;
+                    }
                     }
                     k++;
                 }
