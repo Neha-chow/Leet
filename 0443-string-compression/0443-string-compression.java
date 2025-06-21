7ef839a6795d4c250c5764505b5f64a512e371a4
Neha_E <162294545+Neha-chow@users.noreply.github.com>
2025-06-21 19:49:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,26 +1,20 @@
 class Solution {
     public int compress(char[] chars) {
         StringBuilder sb = new StringBuilder();
-        int count=1;
+        int count = 1;
         for(int i=1;i<chars.length;i++){
             if(chars[i-1]==chars[i]){
                 count++;
             }
             else{
                 sb.append(chars[i-1]);
-                if(count >1)
                 sb.append(count);
-               
-                count=1;
+                count =1;
             }
-        }
-        sb.append(chars[chars.length - 1]);
-        if (count > 1) {
-            sb.append(count);
-        }
-
-       
-        for (int i = 0; i < sb.length(); i++) {
+        } 
+        sb.append(chars[chars.length-1]);
+        if(count>1) sb.append(count);
+       for (int i = 0; i < sb.length(); i++) {
             chars[i] = sb.charAt(i);
         }
 
