5ca2c625e04d5bff251940f907e659349fe457e8
Neha_E <162294545+Neha-chow@users.noreply.github.com>
2025-06-22 16:03:12 +0530

    Time: 6 ms (27.27%), Space: 44.2 MB (92.7%) - LeetHub

--- 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.java
+++ 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.java
@@ -2,27 +2,19 @@ class Solution {
     public int minimumSumSubarray(List<Integer> nums, int l, int r) {
          List<List<Integer>> subarray = new ArrayList<>();
         int n = nums.size();
-
+        int ans=Integer.MAX_VALUE;
+        
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j <= n; j++) {
-                int length = j - i;
-                if (length >= l && length <= r) {
-                    subarray.add(new ArrayList<>(nums.subList(i, j)));
+            int temp = 0;
+            for (int j = i; j < n; j++) {
+                int length = j - i+1;
+                temp += nums.get(j);
+                if (length >= l && length <= r&& temp>0) {
+                  ans= Math.min(temp,ans);  
                 }
             }
         }
 
-        int p = Integer.MAX_VALUE;
-        for (List<Integer> sub : subarray) {
-            int sum = 0; 
-            for (int num : sub) {
-                sum += num;
-            }
-            if (sum < p&& sum>0) {
-                p = sum;
-            }
-        }
-
-        return p == Integer.MAX_VALUE ? -1 : p;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
     }
