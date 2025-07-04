115d4f498bb7b6943b2dab11a92987d4b141ad1c
Neha_E <162294545+Neha-chow@users.noreply.github.com>
2025-07-04 21:41:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        Arrays.sort(stones);
+        // for(int i=stones.length-1;i>0;i--){
+        //     int p = stones[i]-stones[i-1];
+        //     if(p==0){
+        //         i--;
+        //     }
+        //     else{
+
+        //     }
+        // }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<stones.length;i++){
+            pq.offer(stones[i]);
+        }
+        while(pq.size()>1){
+            int p = pq.poll();
+            int q= pq.poll();
+            int r = p-q;
+            if(r!=0){
+                pq.add(r);
+            }
+        }
+        return pq.size==0?0:pq.peek();
+    }
+}
