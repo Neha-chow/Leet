b7418a92a65fa2901a8a28f1c792df7dc787bf68
Neha_E <162294545+Neha-chow@users.noreply.github.com>
2025-07-22 23:46:51 +0530

    Time: 2780 ms (5.3%), Space: 56.7 MB (93.96%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int sum =0,maxsum=0;
+        HashSet<Integer> hm = new HashSet<>();
+        int p1=0,p2=0;
+        for(int i=0;i<nums.length;){
+            if(!hm.contains(nums[i])){
+                hm.add(nums[i]);
+                sum += nums[i];
+                maxsum = Math.max(maxsum,sum);
+                i++;
+            }else{
+                   hm.remove(nums[p1]);
+                    sum -= nums[p1];
+                    p1++;
+                }
+                System.out.println(maxsum);
+                
+            }
+           
+        
+        return maxsum;
+    }
+}
